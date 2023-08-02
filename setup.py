67887c3adb48cff1ad5cd4a641741dbd7678b0d0
PhillipMaire <phillip.maire@gmail.com>
2023-08-02 16:50:36 -0700

    correct requirements file

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='scrapifurs',
-    version='0.1.0',
+    version='0.1.2',
     packages=find_packages(),
     install_requires=required,
     package_data={
@@ -17,5 +17,10 @@ setup(
     },
     # Other metadata here...
 )
+
+
+
+
+
 
 
