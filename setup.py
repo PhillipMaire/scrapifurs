60276876e404e0b745ad90ae394a7f80f30e31d9
PhillipMaire <phillip.maire@gmail.com>
2023-08-14 08:48:22 -0700

    uploaded HTML and new full notebooks

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-
 from setuptools import setup, find_packages
-
-
 
 with open('requirements.txt') as f:
     required = f.read().splitlines()
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()    
 
 
 setup(
@@ -15,8 +15,9 @@ setup(
     package_data={
         'scrapifurs': ['../data/*.txt'],
     },
-    # Other metadata here...
-)
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+    )
 
 
 
